20ea339b6156538088f5cfcd17ea53880311a5af
Ryan <ryanpauley@gmail.com>
2014-11-27 16:46:47 +1100

    completed status list animals.

--- w03/d04/ryan/shelter_manager/app/controllers/animals_controller.rb
+++ w03/d04/ryan/shelter_manager/app/controllers/animals_controller.rb
@@ -1,4 +1,6 @@
 class AnimalsController < ApplicationController
   def index
+  	#@animals is our connection to the database
+  	@animals = Animal.all #gives an array of Animal instances.
   end
 end
