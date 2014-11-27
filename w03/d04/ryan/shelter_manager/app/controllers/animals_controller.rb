a062a1313a636f1c6aa6a1a6915cd76f4178bd1b
Ryan <ryanpauley@gmail.com>
2014-11-27 21:20:04 +1100

    Added Edit Animal Links

--- w03/d04/ryan/shelter_manager/app/controllers/animals_controller.rb
+++ w03/d04/ryan/shelter_manager/app/controllers/animals_controller.rb
@@ -23,6 +23,20 @@ class AnimalsController < ApplicationController
   	end
   end
 
+  def edit
+  	@animal = Animal.find(params[:id])
+  end
+
+  def update
+  	@animal = Animal.find(params[:id])
+
+  	if @animal.update(animal_params)
+  		redirect_to @animal
+  	else
+  		render 'edit'
+  	end
+  end
+  
   private
   	def animal_params
   		params.require(:animal).permit(:name, :breed, :arrival_time, :departure_time, :age, :species, :sex)
