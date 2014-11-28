c0ece9a750a49dedb2775e74ce8190b835a6f6e4
Ryan <ryanpauley@gmail.com>
2014-11-28 13:05:54 +1100

    newest update + git attempt

--- w03/d04/ryan/shelter_manager/app/models/animal.rb
+++ w03/d04/ryan/shelter_manager/app/models/animal.rb
@@ -1,5 +1,5 @@
 class Animal < ActiveRecord::Base
-	validates :name, :species, :breed, :sex, :arrival_time, :departure_time, presence: true, length: {minimum: 3 }
-	validates :age, presence: true, length: {minimum: 1 }
+	validates :name, :species, :breed, :sex, :arrival_time, :departure_time, presence: {strict: true}, length: {minimum: 3 }
+	validates :age, presence: {strict: true}, length: {minimum: 1 }
 
 end
